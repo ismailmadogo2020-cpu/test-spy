f88e6b0c13df5d7d2456b9c2fa14523b73aec4ea
ismail <ismailmadogo2020@gmail.com>
2026-06-22 23:30:52 +0800

    change world to Earth

--- src/Garissa.jsx
+++ src/Garissa.jsx
@@ -2,7 +2,7 @@ function App() {
     return (
         <div>
             <h1>
-                The Universal Food Program will be soon coming to
+                The Earth Food Program will be soon coming to
             </h1>
         </div>
     )
